a2d3f14eac7bfdac8aa9ce03e02a9d80eea12770
Walter <walterr@live.cn>
2023-02-04 00:49:29 +0800

    add document title

--- pages/interview/clipboard.tsx
+++ pages/interview/clipboard.tsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
 
 const ClipboardHistory = () => {
-    const [clipboardHistory, setClipboardHistory] = useState([]);
-  
-    useEffect(() => {
-      const watchClipboard = async () => {
-        const newText = await navigator.clipboard.readText();
-        setClipboardHistory([...clipboardHistory, newText]);
-      };
-  
-      navigator.clipboard.addEventListener('clipboardchange', watchClipboard);
-  
-      return () => {
-        navigator.clipboard.removeEventListener('clipboardchange', watchClipboard);
-      };
-    }, [clipboardHistory]);
-  
-    return (
-      <div>
-        <h3>Clipboard History:</h3>
-        <ul>
-          {clipboardHistory.map((text, index) => (
-            <li key={index}>{text}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-  
-  export default ClipboardHistory;
+  const [clipboardHistory, setClipboardHistory] = useState([]);
+
+  useEffect(() => {
+    document.addEventListener("copy", (event) => {
+      /* the session has shut down */
+      console.log("copy event", event)
+    });
+
+
+    const watchClipboard = async () => {
+      const newText = await navigator.clipboard.readText();
+      setClipboardHistory([...clipboardHistory, newText]);
+    };
+
+    navigator.clipboard.addEventListener('copy', watchClipboard);
+
+    return () => {
+      navigator.clipboard.removeEventListener('copy', watchClipboard);
+      document.removeEventListener("copy", (event) => {
+        /* the session has shut down */
+      });
+
+    };
+  }, [clipboardHistory]);
+
+  return (
+    <div>
+      <h3>Clipboard History:</h3>
+      <ul>
+        {clipboardHistory.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClipboardHistory;
